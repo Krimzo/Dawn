13ffd82548c7e8967a12c5205c3e596025124808
Ivan P. <46162167+Krimzo@users.noreply.github.com>
2025-09-18 18:39:47 +0200

    refactor _main

--- source/_main.cpp
+++ source/_main.cpp
@@ -1,11 +1,87 @@
-#include "_main.h"
+#include "dawn.h"
 
+using namespace dawn; // Only in this case since it's not a header file.
+
+struct Stopwatch
+{
+    using Clock = ch::high_resolution_clock;
+    const Clock::time_point start_time = Clock::now();
+
+    ~Stopwatch() noexcept
+    {
+        const Clock::time_point end_time = Clock::now();
+        const auto duration = ch::duration_cast<ch::microseconds>( end_time - start_time );
+        print( "Elapsed time: ", duration );
+    }
+};
+
+int _dev_main( int argc, char** argv );
+int _shp_main( int argc, char** argv );
 
 int main( int argc, char** argv )
 {
 #ifdef DAWN_SHIP
-    return dawn::_shp_main( argc, argv );
+    return _shp_main( argc, argv );
 #else
-    return dawn::_dev_main( argc, argv );
+    return _dev_main( argc, argv );
 #endif
+}
+
+int _dev_main( int argc, char** argv )
+{
+    Stopwatch stopwatch;
+    Dawn dawn;
+
+    if ( Opt error = dawn.eval_file(
+#if _DEBUG
+#if 0
+        "examples/tests.dw"
+#else
+        "examples/dev.dw"
+#endif
+#else
+        "examples/bench.dw"
+#endif
+    ) )
+    {
+        print( error.value() );
+        return -1;
+    }
+
+    if ( Opt error = dawn.call_func( "main" ) )
+    {
+        print( error.value() );
+        return -2;
+    }
+    return 0;
+}
+
+int _shp_main( int argc, char** argv )
+{
+    if ( argc < 2 )
+    {
+        print( "Usage: dawn <file>" );
+        return -1;
+    }
+
+    Dawn dawn;
+    if ( Opt error = dawn.eval_file( argv[1] ) )
+    {
+        print( error.value() );
+        return -2;
+    }
+
+    ArrayValue arg;
+    for ( int i = 2; i < argc; i++ )
+        arg.data.emplace_back( String{ argv[i] } );
+
+    Value retval{ Int() };
+    if ( Opt error = dawn.call_func( "main", { Value{ arg } }, &retval ) )
+    {
+        print( error.value() );
+        return -3;
+    }
+
+    Int retcode = retval.to_int( dawn.engine );
+    return static_cast<int>( retcode );
 }
