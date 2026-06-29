5ba5a2d98d079929e51496374fb69c6f8d1c87e8
Ivan P. <46162167+Krimzo@users.noreply.github.com>
2026-06-29 20:56:14 +0200

    Update storage.h

--- source/storage.h
+++ source/storage.h
@@ -8,31 +8,24 @@ namespace dawn
 template<typename T>
 struct Storage
 {
-    Storage( size_t initial_size = 0 )
-    {
-        m_data.resize( initial_size );
-    }
+    constexpr Storage()
+    {}
 
     T& set( ID id, T const& t )
     {
-        if ( m_data.size() <= id.integer() )
-            m_data.resize( ( id.integer() + 1 ) * 2 );
+        if ( id.integer() >= m_data.size() )
+            m_data.resize( size_t( id.integer() + 1 ) * 2 );
         return m_data[id.integer()].emplace( t );
     }
 
     T* get( ID id )
     {
-        if ( m_data.size() > id.integer() )
+        if ( id.integer() < m_data.size() )
         {
             if ( auto& opt_obj = m_data[id.integer()] )
                 return &( *opt_obj );
         }
         return nullptr;
-    }
-
-    void clear()
-    {
-        m_data.clear();
     }
 
 private:
